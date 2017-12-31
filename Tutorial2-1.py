aa4bd0e60628f13d7699715d29c2216a62ba8601
Lindsey <34972402+lrsheehan@users.noreply.github.com>
2017-12-31 11:27:48 -0800

    Uploaded Tutorial 2-1

---
+++ Tutorial2-1.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        Tutorial 2-1.py
+# Purpose:     Learn the basic format of Python code used in ArcGIS
+#
+# Author:      lrs
+#
+# Created:     12/31/2017
+# Copyright:   (c) lrs 2017
+#-------------------------------------------------------------------------------
+
+#Import Python moduels
+import arcpy
+from arcpy import env
+
+#Set the workspace environment
+
+#Check the spatial reference for a feature class in the workspace
+
+#Print the results
